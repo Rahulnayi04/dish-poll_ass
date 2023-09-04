aaf49267b4e0f3993a57ccbb00bcd2f0f0312d3c
Rahulnayi04 <112021214+Rahulnayi04@users.noreply.github.com>
2023-09-04 10:56:08 +0530

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom'
 import { HomeWrapper } from '../assets/Wrappers/HomeWrapper'
 import styles from '../assets/styles/home.module.css'
-import Typewriter from 'typewriter-effect'
 import { Navbar } from './'
 function DashBoard() {
   return (
@@ -22,22 +21,7 @@ function DashBoard() {
             Show Result
           </Link>
         </div>
-        {/* welcome text on dashboard */}
-        {/* <Typewriter
-          options={{
-            strings: [
-              'Welcome To Dish Voting App!',
-              'Start Voting Your Favourite dishes!',
-            ],
-            autoStart: true,
-            loop: true,
-            deleteSpeed: 1,
-            delay: 40,
-            wrapperClassName: styles.welcomeText,
-            cursorClassName: styles.cursor,
-            pauseFor: 2500,
-          }}
-        /> */}
+        
       </HomeWrapper>
     </div>
   )
